2dc336e2c2f5693090d5d644e7744168c8089411
Petrik Diak <diak@petrik.hu>
2023-11-06 17:11:33 +0100

    1 feladat

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 import './style.css'
 import array from './quotes.json'
 
+
+document.addEventListener('DOMContentLoaded',init)
 let a1 = Array(array.quotes)
 function osszes(){
 
-console.log(a1)
+const list = document.getElementById('myList');
+let a2 = Array.from(array.quotes)
+ 
+a2.sort((a,b)=>{
+  if(a.author < b.author)
+  {
+    return -1;
+  }
+})
+
+
+
+a2.forEach((a)=>{
+  let sor = document.createElement('li')
+  sor.textContent
+  list.appendChild(sor);
+})
+
+
+
+
+
+
+
 }
 
+function init(){
+  const osszesGomb =document.getElementById('osszes');
+  osszesGomb.addEventListener('click',osszes);
+}
